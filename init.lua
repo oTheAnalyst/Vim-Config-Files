0e16192dfa9688c0315537f3232e5b19e8932756
Unfixable47 <Unfixable47@proton.me>
2023-12-18 08:21:43 -0500

    added LaTeX functionality with vimtex now LaTeX is configurable with R

--- init.lua
+++ init.lua
@@ -4,6 +4,23 @@ require("plugins.Nvim-R")
 require("plugins.ncm-R")
 require("plugins.open-browser")
 require("plugins.vimtex")
+require("plugins.tokyonight")
 require("lspconfig").r_language_server.setup({})
+require("lspconfig").texlab.setup({})
 
 vim.o.clipboard = "unnamedplus"
+vim.cmd([[colorscheme tokyonight-night]])
+
+vim.g.vimtex_view_method = "zathura"
+
+local function insert_data_report_template()
+  local template_path = "/home/pretender/LaTeX/templates/data_analysis_template.tex" -- Replace with your actual full path
+  local lines = vim.fn.readfile(template_path)
+  if #lines == 0 then
+    print("Template file not found: " .. template_path)
+    return
+  end
+  vim.api.nvim_buf_set_lines(0, 0, 0, false, lines)
+end
+
+vim.api.nvim_create_user_command("DataReportTemplate", insert_data_report_template, {})
--- lua/plugins/Nvim-R.lua
+++ lua/plugins/Nvim-R.lua
@@ -1,13 +1,20 @@
 return {
-  -- Add Nvim-R
   {
     "jalvesaq/Nvim-R",
     keys = { { "<leader>bf", cmd = "Start Nvim-R" } }, -- Corrected key mapping
     ft = { "r", "rmd" },
     cmd = { "R" }, -- Ensure this is a table
     config = function()
-      -- Display a message when the plugin is loaded
+      -- Display a message when the plugin is initialized
       print("Nvim-R plugin is initialized")
+
+      -- Auto-command to open R console when Nvim-R starts
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "NvimRStarted",
+        callback = function()
+          vim.cmd("StartR")
+        end,
+      })
     end,
   },
 }
