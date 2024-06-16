4508b9352fb6cc225ce87dc80aabec8347d492d5
VtheRtech <sakeyisu@gmail.com>
2024-06-15 21:33:01 -0400

    removed language server

--- init.lua
+++ init.lua
@@ -2,7 +2,5 @@ require("config.lazy")
 require("plugins.ncm-R")
 require("plugins.cmp-r")
 require("plugins.r-nvim")
--- setup language server
-require("lspconfig").rlanguage_server.setup({})
 
 vim.cmd([[colorscheme habamax]])
